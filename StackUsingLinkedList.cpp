a98f9d434f360b6b1327a371185054e3f5f0b217
xyerakovalskia <xyera123987@gmail.com>
2026-05-12 11:07:54 +0700

    menambah fungsi int push

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -26,3 +26,12 @@ public:
     {
         top = NULL;
     }
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
