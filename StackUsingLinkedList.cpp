01126446528d68da8852f92112880049459cdede
xyerakovalskia <xyera123987@gmail.com>
2026-05-12 11:16:36 +0700

    menambahkan while di int main

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -82,3 +82,12 @@ int main()
 
     int choice = 0;
     int value;
+
+    while (choice !=5)
+    {
+        cout << "1.push\n";
+        cout << "2.pop\n";
+        cout << "3.peek\n";
+        cout << "4.exit\n";
+        cout << "enter your choice:";
+        cin >> choice;
