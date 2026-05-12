bd37bd17b1c8e2092ff889691e26cdb618aab1dd
xyerakovalskia <xyera123987@gmail.com>
2026-05-12 11:03:23 +0700

    membuat class

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 
 using namespace std;
+
+//node class representing a single node in the linked list
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
