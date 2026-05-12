14986b8e82e8f90484525dd53fca2af172e4ddb4
xyerakovalskia <xyera123987@gmail.com>
2026-05-12 11:01:28 +0700

    first commit

---
+++ StackUsingLinkedList.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+
+using namespace std;
+
+//node class representing a single node in the linked list
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
+
+//Stack class
+class stack
+{
+private:
+Node *top; // pointer to the top node of the stack
+
+public:
+    stack()
+    {
+        top = NULL;
+    }
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+    // pop operation: Remove the topmost element from the stack
+    void pop()
+    {
+        if (isempty())
+        {
+            cout << "Stack is empty." << endl;
+        }
+
+        Node *temp = top;
+        top = top->next;
+        cout << "Popped value: " << top->data << endl;
+    }
+
+    //peel/top operation: Retrive the value of the topset element without removing 
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is empthy." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << "  " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        
+        } // return the value of the top node
+    }
+
+    //is empthy operation: check of the stack is empthy
+    bool isempty()
+    {
+        return top == NULL; //return true if the top pointer is null, indicatig an empty stack
+    }
+};
+
+int main()
+{
+    stack stack;
+
+    int choice = 0;
+    int value;
+
+    while (choice !=5)
+    {
+        cout << "1.push\n";
+        cout << "2.pop\n";
+        cout << "3.peek\n";
+        cout << "4.exit\n";
+        cout << "enter your choice:";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "enter the value to push:";
+            cin >> value;
+            stack.push(value); //push the entered value into the stack
+            break;
+        case 2:
+            if (!stack.isempty())
+            {
+                stack.pop(); //pop the top element from the stack
+            }
+            else
+            {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
+        case 3:
+            if (!stack.isempty())
+            {
+                stack.peek();
+            }
+            else
+            {
+                 cout << "Stack is empty. No top value." << endl;
+            }
+            break;
+
+        case 4:
+            cout << "exiting program." << endl;
+            break;
+        default:
+            cout << "invalid choice. try again." << endl;
+            break;
+        }
+        cout << endl;
+    }
+    return 0;
+}
