11993fafba9f033118227a073be2c1d1cafe48bf
xyerakovalskia <xyera123987@gmail.com>
2026-05-12 11:10:11 +0700

    isi fungsi void pop

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -43,3 +43,8 @@ public:
         {
             cout << "Stack is empty." << endl;
         }
+
+        Node *temp = top;
+        top = top->next;
+        cout << "Popped value: " << top->data << endl;
+    }
